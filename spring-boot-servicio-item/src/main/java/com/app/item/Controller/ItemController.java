d497a1733595076b9daedc5081df97bde4fd57c3
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-18 16:40:40 -0500

    controladoer

---
+++ spring-boot-servicio-item/src/main/java/com/app/item/Controller/ItemController.java
@@ -0,0 +1,30 @@
+package com.app.item.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.item.models.Item;
+import com.app.item.service.ItemService;
+
+@RestController
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@GetMapping("/Get")
+	public List<Item> get(){
+		return itemService.findAll();	
+	}
+	
+	@GetMapping("/Get/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id , @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+		
+	}
+	
+}
