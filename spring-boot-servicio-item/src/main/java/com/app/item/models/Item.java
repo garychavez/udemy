9b811ea0cabb17f490b76d1e095f1bd6df6d0adf
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-18 15:16:01 -0500

    clases producto e item

---
+++ spring-boot-servicio-item/src/main/java/com/app/item/models/Item.java
@@ -0,0 +1,35 @@
+package com.app.item.models;
+
+public class Item {
+ private Producto producto;
+ private Integer cantidad;
+ 
+ // constructores
+public Item() {}
+
+
+public Item(Producto producto, int cantidad) {
+	this.producto = producto;
+	this.cantidad = cantidad;
+}
+
+
+//Getters and Setters
+public Producto getProducto() {
+	return producto;
+}
+public void setProducto(Producto producto) {
+	this.producto = producto;
+}
+public int getCantidad() {
+	return cantidad;
+}
+public void setCantidad(int cantidad) {
+	this.cantidad = cantidad;
+}
+ 
+ 
+public Double getTotal() {
+	return producto.getPrecio() *  cantidad.doubleValue();
+}
+}
