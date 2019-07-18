23d1c4c68f2af0418b018c1bbe4bd157dcda239e
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-18 16:18:01 -0500

    item service implement

--- spring-boot-servicio-item/src/main/java/com/app/item/serviceImplement/ItemServiceImplwement.java
+++ spring-boot-servicio-item/src/main/java/com/app/item/serviceImplement/ItemServiceImplwement.java
@@ -1,12 +1,18 @@
 package com.app.item.serviceImplement;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.app.item.models.Item;
+import com.app.item.models.Producto;
 import com.app.item.service.ItemService;
 
 @Service
@@ -16,14 +22,17 @@ public class ItemServiceImplwement implements ItemService {
 
 	@Override
 	public List<Item> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Producto> productos= Arrays.asList(clienteRest.getForObject("http://localhost:8001/swagger-ui.html#/producto-controller/findAllUsingGET", Producto[].class));
+		return productos.stream().map(p -> new Item(p,1)).collect(Collectors.toList());
 	}
 
 	@Override
 	public Item findById(Long id, Integer cantidad) {
-		// TODO Auto-generated method stub
-		return null;
+Map<String , String> pathVariable = new HashMap<String , String>();
+pathVariable.put("id",id.toString() )	;
+Producto producto= clienteRest.getForObject("http://localhost:8001/swagger-ui.html#/producto-controller/getUsingGET", Producto.class,pathVariable);
+
+		return new Item(producto,cantidad);
 	}
 
 }
